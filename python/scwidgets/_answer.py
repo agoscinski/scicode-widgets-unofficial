d154b5dc0df4526e7353e2f2b922f5aef2bd7e5d
jprado1 <98404937+jprado1@users.noreply.github.com>
2022-12-10 23:43:10 +0100

    streamline confirmation request

--- python/scwidgets/_answer.py
+++ python/scwidgets/_answer.py
@@ -15,6 +15,30 @@ from ipywidgets import (
     Dropdown
 )
 
+#TODO put this helper in a more meaningful place
+def request_confirmation(output,callback=None,warning=None,change=""):
+    """Callback wrapper that implements confirmation/cancel button before a given action/method. 
+    output : The output that will display Confirmation/Cancel buttons (and warnings if any are defined).
+    callback : Callback to be called if confirmation button is clicked. If None, confirmation button has the same behaviour as cancel button.
+    warning : Message to be shown with buttons, indicating to user the effect of confirming
+    """
+    def collapse_after_confirmation(callback=None,change=""):
+        if callback != None:
+            callback()
+        else :
+            output.clear_output()
+    confirm_button = Button(description="Yes")
+    cancel_button = Button(description="Cancel")
+    confirm_button.on_click(lambda _: collapse_after_confirmation(callback))
+    cancel_button.on_click(lambda _: collapse_after_confirmation())
+    with output:
+        output.clear_output()
+        if warning == None:
+            warning = "Are you sure ?"
+        print(warning)
+        with output:
+            display(HBox([confirm_button,cancel_button]))
+
 class AnswerStatus(enum.Enum):
     NOT_SAVED = 0
     SAVED = 1
@@ -75,27 +99,10 @@ class Answer:
         self._reload_answer_button = Button(description="Reload saved answer", layout=Layout(width="200px", height="100%"),disabled = True)
         self._on_save_callback = callback
         self._save_button.on_click(self._on_save_callback)
-        self._reload_answer_button.on_click(self._request_reload_confirmation)
+        self._reload_answer_button.on_click(lambda _ : request_confirmation(self._save_output,self.on_reload_callback))
         return VBox([VBox([self._save_button,self._reload_answer_button]), self._save_output],
                 layout=Layout(align_items="flex-start")
         )
-
-    def _request_reload_confirmation(self,change=""):
-        def collapse_after_confirmation(callback=None,change=""):
-            if callback != None:
-                callback()
-                self._save_output.clear_output()
-            else :
-                self._save_output.clear_output()
-        confirm_button = Button(description="Yes")
-        cancel_button = Button(description="Cancel")
-        confirm_button.on_click(lambda _: collapse_after_confirmation(self.on_reload_callback))
-        cancel_button.on_click(lambda _: collapse_after_confirmation())
-        with self._save_output:
-            self._save_output.clear_output()
-            print("Are you sure ? This will reload irreversibly your last saved answer.")
-            with self._save_output:
-                display(HBox([confirm_button,cancel_button]))
 
     def _update_save_widget(self, callback):
         if self._save_button is not None and self._on_save_callback is not None:
@@ -153,7 +160,7 @@ class AnswerRegistry(VBox):
         self._preoutput = Output(layout=Layout(width='99%', height='99%'))
         self._output = Output(layout=Layout(width='99%', height='99%'))
         self._save_all_button = Button(description = 'Save all answers')
-        self._save_all_button.on_click(self._request_confirmation)
+        self._save_all_button.on_click(lambda _ : request_confirmation(self._output,self._save_all))
         super().__init__(
                 [self._preoutput, self._savebox, self._output])
 
@@ -274,29 +281,14 @@ class AnswerRegistry(VBox):
                 print(f"The answer was successfully recorded in '{self._answers_filename}'")
             return True
 
-    def _request_confirmation(self,change=""):
-        def collapse_after_confirmation(callback=None,change=""):
-            if callback != None:
-                callback()
-                self.clear_output()
-                with self._output : 
-                    print(f"All answers were successfully recorded in '{self._answers_filename}'")
-            else :
-                self.clear_output()
-            self.children = [self._savebox, HBox([self._reload_button, self._save_all_button]), self._output]
-        confirm_button = Button(description="Yes")
-        cancel_button = Button(description="Cancel")
-        confirm_button.on_click(lambda _: collapse_after_confirmation(self._save_all))
-        cancel_button.on_click(lambda _: collapse_after_confirmation())
-        with self._output:
-            self.clear_output()
-            print("Are you sure ? This will modify irreversibly your answer file.")
-            self.children = [self._savebox, HBox([self._reload_button, self._save_all_button]), HBox([self._output, confirm_button,cancel_button])]        
-
     def _save_all(self,change=None):
-        for answer_key, widgets in self._answer_widgets.items():
+        self._output.clear_output()
+        for widgets in self._answer_widgets.values():
             widgets._save_button.click()
+        with self._output : 
+            print(f"All answers were successfully recorded in '{self._answers_filename}'")
 
+        
     @staticmethod
     def is_name_empty(name):
         return len(name) == name.count(" ")
